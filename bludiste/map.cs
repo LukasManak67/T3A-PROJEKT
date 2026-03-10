df2356563e93b8da5cce7fba3e4c6e3e4aa2efd8
Kluci <Kluci@DESKTOP-83R2E1S>
2026-03-10 06:26:56 +0100

    Přidejte soubory projektu.

---
+++ bludiste/map.cs
@@ -0,0 +1,73 @@
+﻿using System.Drawing;
+
+namespace bludiste
+{
+    public class Map
+    {
+        public int TileSize = 50;
+        public int Width = 50;
+        public int Height = 50;
+
+        public int[,] grid;
+
+        public Map()
+        {
+            grid = new int[Height, Width];
+
+            GenerateBorders();
+            GenerateMaze();
+
+        }
+
+        private void GenerateBorders()
+        {
+            for (int radek  = 0; radek < Height; radek++)
+            {
+                for (int sloupec = 0; sloupec < Width; sloupec++)
+                {
+                    if (radek == 0 || radek == Height - 1 ||
+                        sloupec == 0 || sloupec == Width - 1)
+                    {
+                        grid[radek, sloupec] = 1; // zeď
+                    }
+                    else
+                    {
+                        grid[radek, sloupec] = 0; // volno
+                    }
+                }
+            }
+        }
+
+        public void Draw(Graphics g, int cameraX, int cameraY)
+        {
+            for (int radek = 0; radek < Height; radek++)
+            {
+                for (int sloupec = 0; sloupec < Width; sloupec++)
+                {
+                    Brush brush = grid[radek, sloupec] == 1
+                        ? Brushes.DarkGray
+                        : Brushes.LightGray;
+
+                    g.FillRectangle(
+                        brush,
+                        sloupec * TileSize - cameraX,
+                        radek * TileSize - cameraY,
+                        TileSize,
+                        TileSize
+                    );
+                }
+            }
+        }
+        private void GenerateMaze()
+        {
+            for (int radek = 2; radek < Height - 2; radek += 2)
+            {
+                for (int sloupec = 2; sloupec < Width - 2; sloupec += 2)
+                {
+                    grid[radek, sloupec] = 1;
+                }
+            }
+        }
+
+    }
+}
---
+++ bludiste/player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bludiste
+{
+    public class Player
+    {
+        public float X;
+        public float Y;
+        private bool isDashing = false;
+        private int dashTimer = 0;
+        private int dashDuration = 10;
+        public float NSpeed = 5f;
+        public float DSpeed = 20f;
+        public int Size = 40;
+
+        public Player(int startX, int startY)
+        {
+            X = startX;
+            Y = startY;
+        }
+
+        public void Update(bool up, bool down, bool left, bool right)
+        {
+            float currentSpeed = NSpeed;
+
+            if (isDashing)
+            {
+                currentSpeed = DSpeed;
+                dashTimer--;
+
+                if (dashTimer <= 0)
+                {
+                    isDashing = false;
+                }
+            }
+
+            if (up) Y -= currentSpeed;
+            if (down) Y += currentSpeed;
+            if (left) X -= currentSpeed;
+            if (right) X += currentSpeed;
+        }
+
+        public void Draw(Graphics g, int cameraX, int cameraY)
+        {
+            g.FillRectangle(Brushes.Blue,X - cameraX,Y - cameraY,Size,Size);
+
+        }
+        public void StartDash()
+        {
+            if (!isDashing)
+            {
+                isDashing = true;
+                dashTimer = dashDuration;
+            }
+        }
+
+    }
+}
